447177f4c7536aa32d5d444d846a4f6ed432f902
Asadujjaman Julhas <fahim0760@gmail.com>
2022-05-24 01:46:57 +0600

    Store review on server

--- src/pages/CTA/CTA.js
+++ src/pages/CTA/CTA.js
@@ -1,19 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './CTA.css'
+import OwlCarousel from 'react-owl-carousel';  
+import 'owl.carousel/dist/assets/owl.carousel.css';  
+import 'owl.carousel/dist/assets/owl.theme.default.css'; 
+import { Carousel } from 'react-bootstrap';
+import axios from 'axios';
 
 const CTA = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/reviews')
+    .then(res => {
+      setReviews(res.data)
+    })
+  },[])
+
   return (
     <section className="sale-offer-area">
 			<div className="container">
-				<div className="sale-offer-bg bg-2">
+				<div className="row">
+          <div className="col-md-3">
+          <div>  
+          <div className='container-fluid' >      
+           <div className="row" style={{marginBottom: "5px"}}>      
+           <div className="col-sm-12 btn btn-info reviewBanner text-white">      
+          Reviews from our clients 
+           </div>      
+           </div>  
+       </div>  
+       <div className='container-fluid' >            
+        <OwlCarousel items={1}  
+          className="owl-theme"  
+          loop  
+          nav  
+          margin={8} >  
+          {reviews.map(r => 
+          <div className='text-center' key={r._id}>
+            <img className="img" src={r.img} alt='' /> <br />
+            <h4>{r.name}</h4>
+            <p>{r.comments}</p>
+            <span className='rating'>{r.rating}.00 start</span>
+          </div> )}
+      </OwlCarousel>  
+      </div>  
+  
+      </div>
+
+
+
+
+
+          </div>
+          <div className="col-md-9">
+          <div className="sale-offer-bg bg-2">
 					<h5>Sale offer - <span>30% off</span></h5>
 					<h3>All types of premium quality tools</h3>
-					<a href="" className="default-btn">
+					<Link to='/' className="default-btn">
 						<i className="ri-shopping-cart-line"></i>
 					Book a order
-					</a>
+					</Link>
 				</div>
+          </div>
+        </div>
 			</div>
+      <script>
+        
+      </script>
 		</section>
   );
 };
--- src/pages/Dashboard/Review.js
+++ src/pages/Dashboard/Review.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import axios from "axios";
+import { signOut } from "firebase/auth";
+import React from "react";
+import { Button, FloatingLabel, Form } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { toast } from "react-toastify";
+import auth from "../../firebaseinit";
 
 const Review = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const accessToken = localStorage.getItem("accessToken");
+  const handleRating = (e) => {
+    e.preventDefault();
+    const comments = e.target.comments.value;
+    const rating = e.target.rating.value;
+    const name = user.displayName;
+    const email = user.email;
+    const img = "https://i.postimg.cc/HsrDdSnc/index.jpg";
+
+    const data = { comments, rating, name, email, img };
+    axios.post("http://localhost:5000/review", data, {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((res) => {
+        if (res.data.message) {
+          signOut(auth);
+          localStorage.removeItem("accessToken");
+        } else if (res.data.insertedId) {
+          toast.success("Review added successfully!", {
+            position: "top-center",
+          });
+          e.target.reset();
+        }
+      });
+  };
+
   return (
     <div>
-      My review
+      <h3 className="fs-6 mb-2">Add a Review</h3>
+      <Form onSubmit={handleRating} className="w-50">
+        <FloatingLabel controlId="floatingTextarea2" label="Comments">
+          <Form.Control
+            name="comments"
+            as="textarea"
+            placeholder="Leave a comment here"
+            style={{ height: "100px" }}
+          />
+        </FloatingLabel>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Disabled select menu</Form.Label>
+          <Form.Select name="rating">
+            <option disabled>Your Your rating</option>
+            <option value="5">5 out of 5</option>
+            <option value="4">4 out of 5</option>
+            <option value="3">3 out of 5</option>
+            <option value="2">2 out of 5</option>
+            <option value="1">1 out of 5</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
